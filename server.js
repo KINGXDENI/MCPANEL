636e41eff5ad11a64cdfb76a200978a86929901e
KINGBOTs1 <ds767365@gmail.com>
2024-07-31 14:00:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8900;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
